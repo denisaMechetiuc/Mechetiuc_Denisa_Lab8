5f7dbad75fb2297d308dab08eb9e925abbfa2178
DENISA-NASTACA MECHETIUC <denisa.mechetiuc@stud.ubbcluj.ro>
2021-12-10 10:23:08 +0200

    Laborator 9

--- Pages/Books/Create.cshtml.cs
+++ Pages/Books/Create.cshtml.cs
@@ -10,7 +10,7 @@ using Mechetiuc_Denisa_Lab8.Models;
 
 namespace Mechetiuc_Denisa_Lab8.Pages.Books
 {
-    public class CreateModel : PageModel
+    public class CreateModel : BookCategoriesPageModel
     {
         private readonly Mechetiuc_Denisa_Lab8.Data.Mechetiuc_Denisa_Lab8Context _context;
 
@@ -22,6 +22,10 @@ namespace Mechetiuc_Denisa_Lab8.Pages.Books
         public IActionResult OnGet()
         {
             ViewData["PublisherID"] = new SelectList(_context.Set<Publisher>(), "ID", "PublisherName");
+            var book = new Book();
+            book.BookCategories = new List<BookCategory>();
+            PopulateAssignedCategoryData(_context, book);
+
             return Page();
         }
 
@@ -30,13 +34,40 @@ namespace Mechetiuc_Denisa_Lab8.Pages.Books
 
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://aka.ms/RazorPagesCRUD.
-        public async Task<IActionResult> OnPostAsync()
+
+    public async Task<IActionResult> OnPostAsync(string[] selectedCategories)
         {
+            var newBook = new Book();
+            if (selectedCategories != null)
+            {
+                newBook.BookCategories = new List<BookCategory>();
+                foreach (var cat in selectedCategories)
+                {
+                    var catToAdd = new BookCategory
+                    {
+                        CategoryID = int.Parse(cat)
+                    };
+                    newBook.BookCategories.Add(catToAdd);
+                }
+            }
+
+            if (await TryUpdateModelAsync<Book>(
+                newBook,
+                "Book",
+                i => i.Title, i => i.Author, 
+                i => i.Price, i => i.PublishingDate, i => i.PublisherID))
+            {
+                _context.Book.Add(newBook);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
+            }
+            PopulateAssignedCategoryData(_context, newBook);
+            return Page();
+
             if (!ModelState.IsValid)
             {
                 return Page();
             }
-
             _context.Book.Add(Book);
             await _context.SaveChangesAsync();
 
--- Pages/Books/Edit.cshtml.cs
+++ Pages/Books/Edit.cshtml.cs
@@ -7,11 +7,12 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Mechetiuc_Denisa_Lab8.Data;
+using Mechetiuc_Denisa_Lab8;
 using Mechetiuc_Denisa_Lab8.Models;
 
 namespace Mechetiuc_Denisa_Lab8.Pages.Books
 {
-    public class EditModel : PageModel
+    public class EditModel : BookCategoriesPageModel
     {
         private readonly Mechetiuc_Denisa_Lab8.Data.Mechetiuc_Denisa_Lab8Context _context;
 
@@ -30,25 +31,61 @@ namespace Mechetiuc_Denisa_Lab8.Pages.Books
                 return NotFound();
             }
 
-            Book = await _context.Book.FirstOrDefaultAsync(m => m.ID == id);
+            Book = await _context.Book
+                .Include(b => b.Publisher)
+                .Include(b => b.BookCategories).ThenInclude(b => b.Category)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.ID == id);
 
             if (Book == null)
             {
                 return NotFound();
             }
+
+            PopulateAssignedCategoryData(_context, Book);
+
             ViewData["PublisherID"] = new SelectList(_context.Set<Publisher>(), "ID", "PublisherName");
             return Page();
         }
 
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://aka.ms/RazorPagesCRUD.
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync(int? id, string[] selectedCategories)
         {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var bookToUpdate = await _context.Book
+               .Include(i => i.Publisher)
+               .Include(i => i.BookCategories)
+                   .ThenInclude(i => i.Category)
+               .FirstOrDefaultAsync(s => s.ID == id);
+
+            if (bookToUpdate == null)
+            {
+                return NotFound();
+            }
+
+            if (await TryUpdateModelAsync<Book>(
+                bookToUpdate,
+                "Book",
+                i => i.Title, i => i.Author,
+                i => i.Price, i => i.PublishingDate, i => i.Publisher))
+            {
+                UpdateBookCategories(_context, selectedCategories, bookToUpdate);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
+            }
+
+            UpdateBookCategories(_context, selectedCategories, bookToUpdate);
+            PopulateAssignedCategoryData(_context, bookToUpdate);
+            return Page();
+
             if (!ModelState.IsValid)
             {
                 return Page();
             }
-
             _context.Attach(Book).State = EntityState.Modified;
 
             try
